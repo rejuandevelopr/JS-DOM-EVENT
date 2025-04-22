baac832cebeb288a2fdf692324923fcf9a58520e
S.M.Rejuanul Islam <rejuanhossain8@gmail.com>
2025-04-23 03:38:17 +0600

    1st

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent  = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.score').value = 23;
+// console.log(document.querySelector('.score').value);
+
+// document.querySelector('.check').addEventListener('click', function (){
+//     console.log(document.querySelector('.guess').value);
+
+// })
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = document.querySelector('.guess').value;
+  console.log(typeof guess);
+});
