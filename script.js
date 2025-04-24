2a6fe954209b9a944392f3fb0fb7a0407538a202
S.M.Rejuanul Islam <rejuanhossain8@gmail.com>
2025-04-25 02:04:32 +0600

    3rd

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'Correct Number!';
-console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent  = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent  = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.score').value = 23;
-console.log(document.querySelector('.score').value);
+// document.querySelector('.score').value = 23;
+// console.log(document.querySelector('.score').value);
 
-document.querySelector('.check').addEventListener('click', function (){
-    console.log(document.querySelector('.guess').value);
+// document.querySelector('.check').addEventListener('click', function (){
+//     console.log(document.querySelector('.guess').value);
 
-})
+// })
+
+const number = Math.trunc(Math.random()*20) + 1;
+
+document.querySelector('.number').textContent = number;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -24,3 +28,6 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No Number!';
   }
 });
+
+
+
